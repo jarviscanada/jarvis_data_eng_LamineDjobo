109da769e28ba7b5a5161a80d190c9676c64bf6e
Lamine Djobo <laminedjobo@hotmail.fr>
2024-08-09 10:11:47 +0000

    sql/ddl.sql script created

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,28 @@
+-- Switch to the host_agent database
+-- \c host_agent redondan since the excecution below -d will access the database automatically
+
+-- Create the host_info table if it does not exist
+CREATE TABLE IF NOT EXISTS host_info (
+    id SERIAL PRIMARY KEY,
+    hostname VARCHAR(255) NOT NULL,
+    cpu_number INTEGER NOT NULL,
+    cpu_architecture VARCHAR(50) NOT NULL,
+    cpu_model VARCHAR(255) NOT NULL,
+    cpu_mhz NUMERIC NOT NULL,
+    l2_cache INTEGER NOT NULL,
+    total_mem INTEGER NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the host_usage table if it does not exist
+CREATE TABLE IF NOT EXISTS host_usage (
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    memory_free INTEGER NOT NULL,
+    cpu_idle INTEGER NOT NULL,
+    cpu_kernel INTEGER NOT NULL,
+    disk_io INTEGER NOT NULL,
+    disk_available INTEGER NOT NULL,
+    host_id INTEGER REFERENCES host_info(id)
+);
+
