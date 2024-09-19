5329d6140019b094738c372249dee3f546a04a65
lamine005 <laminedjobo@hotmail.fr>
2024-09-19 09:43:02 -0400

    completed java doc for javagrep interface

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface JavaGrep {
     /**
      * Top-level method to start the processing of files.
-     * @throws IOException
+     * @throws IOException if an I/O error occurs during file reading or writing
      */
     void process() throws IOException;
 
@@ -26,53 +26,59 @@ public interface JavaGrep {
     List<String> readLines (String inputFile);
 
     /**
-     *
-     * @param line
-     * @return
+     * Check if a given line contains the pattern specified by the regex.
+     * @param line the line of text to check against the regex pattern
+     * @return true if the line matches the pattern, false otherwise
      */
     boolean containsPattern (String line);
 
     /**
-     *
-     * @param line
-     * @throws IOException
+     * Write the list of matched lines to the output file.
+     * @param line the list of lines to write to the output file
+     * @throws IOException if an I/O error occurs during file writing
      */
     void writeToFile (List<String> line) throws IOException;
 
     /**
+     * Get the root path where the file search starts.
      *
-     * @return
+     * @return the root directory path as a string
      */
     String getRoothPath();
 
     /**
+     * Set the root path where the file search should start.
      *
-     * @param rootPath
+     * @param rootPath rootPath the root directory path as a string
      */
     void setRootPath(String rootPath);
 
     /**
+     * Get the regular expression pattern to use for searching within files.
      *
-     * @return
+     * @return regex the regex pattern as a string
      */
     String getRegex();
 
     /**
+     * Set the regular expression pattern to use for searching within files.
      *
-     * @param regex
+     * @param regex regex the regex pattern as a string
      */
     void setRegex(String regex);
 
     /**
+     * Get the output file path where the results will be written.
      *
-     * @return
+     * @return the output file path as a string
      */
     String getOutFile() ;
 
     /**
+     * Set the output file path where the results will be written.
      *
-     * @param outFile
-     * @return
+     * @param outFile the output file path as a string
+
      */
     Void setOutFile (String outFile);
 }
---
+++ core_java/learning.jdbc/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.lamine.lil.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
